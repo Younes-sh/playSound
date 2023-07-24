090626d827757ea2451b5b4190e5c15ce0445760
Younes-sh <sheikhlaryounes@gmail.com>
2023-07-24 19:04:04 +0200

    Update App.js

--- play/src/App.js
+++ play/src/App.js
@@ -1,16 +1,25 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Routing from './Routes/Routing';
+import Navbar from './Components/Navbar';
+import Task from './Components/Task';
+import Header from './Components/Header';
 function App() {
   return (
-    <div className="App">
+    <div className="App  ">
       <BrowserRouter>
-        <div className='SideLeft'></div>
-        <div className='main'>
-          <Routing/>
-        </div>
-        <div className='SideRight'>
+        <div className='GlaceMorphosm d-flex'>
+          <div className='SideLeft  ' style={{width:'15%'}} >
+            <Navbar />
+          </div>
+          <div className='main   ' style={{width:'70%'}} >
+            <Task />
+            <Header />
+            <Routing/>
+          </div>
+          <div className='SideRight  ' style={{width:'15%'}} >
 
+          </div>
         </div>
         
       </BrowserRouter>

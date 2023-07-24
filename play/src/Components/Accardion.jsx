792060041016381e3e9b826eeb6ebaa898f6570b
Younes-sh <sheikhlaryounes@gmail.com>
2023-07-24 12:37:12 +0200

    created Components Navbar and Accardion

---
+++ play/src/Components/Accardion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Accardion = () => {
+  return (
+    <div>Accardion</div>
+  )
+}
+
+export default Accardion
---
+++ play/src/Components/Cards/CardSmall.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardSmall = () => {
+  return (
+    <div>CardSmall</div>
+  )
+}
+
+export default CardSmall

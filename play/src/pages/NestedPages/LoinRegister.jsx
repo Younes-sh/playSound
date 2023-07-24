89e7c1c66d4f53be8754c5fe89bde91b3066fedb
Younes-sh <sheikhlaryounes@gmail.com>
2023-07-24 12:38:13 +0200

    create base the pages

---
+++ play/src/pages/NestedPages/LoinRegister.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoinRegister = () => {
+  return (
+    <div>
+        <h1>Loin & Register</h1>
+    </div>
+  )
+}
+
+export default LoinRegister
---
+++ play/src/pages/Podcasts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Podcasts = () => {
+  return (
+    <div>
+        <h1>Podcasts</h1>
+    </div>
+  )
+}
+
+export default Podcasts

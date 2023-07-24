307d2453b38921b5cac4436fd1e828a717c189db
Younes-sh <sheikhlaryounes@gmail.com>
2023-07-24 17:02:36 +0200

    edit component Navbar

--- play/src/Components/Navbar.jsx
+++ play/src/Components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Link , NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='d-flex flex-column  p-5'>
+      <div className='Nav d-flex flex-column text-start py-5 '>
+        <NavLink className='text-decoration-none py-1' to='/home'>Home</NavLink>
+        <NavLink className='text-decoration-none py-1' to='/library'>Library</NavLink>
+        <NavLink className='text-decoration-none py-1' to='/musicvideo'>Music Video</NavLink>
+        <NavLink className='text-decoration-none py-1' to='/podcasts'>Podcasts</NavLink>
+        <NavLink className='text-decoration-none py-1' to='/radio'>Radio</NavLink>
+      </div>
+
+      <div className=' d-flex flex-column text-start py-5 '>
+        <NavLink className='text-decoration-none py-1'>Recently Played</NavLink>
+        <NavLink className='text-decoration-none py-1'>Favorite Song</NavLink>
+        <NavLink className='text-decoration-none py-1'>Album</NavLink>
+        <NavLink className='text-decoration-none py-1'>Artist</NavLink>
+      </div>
+
+      <div className=' d-flex flex-column text-start py-5 '>
+        <NavLink className='text-decoration-none py-1'>My Favorite Song</NavLink>
+        <NavLink className='text-decoration-none py-1'>Music of Summer</NavLink>
+        <NavLink className='text-decoration-none py-1'>Today's Top Hits</NavLink>
+      </div>
+    </div>
   )
 }
 
